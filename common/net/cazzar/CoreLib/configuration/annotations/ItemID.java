f5d7e5f7461df0df7b5d754984a78efef81be709
Cayde Dixon <cayde.dixon@gmail.com>
2013-07-23 17:50:20 +1000

    Added the correct annotations.

--- common/net/cazzar/CoreLib/configuration/annotations/ItemID.java
+++ common/net/cazzar/CoreLib/configuration/annotations/ItemID.java
@@ -17,5 +17,13 @@
 
 package net.cazzar.corelib.configuration.annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target(TYPE)
+@Retention(RUNTIME)
 public @interface ItemID {
 }
