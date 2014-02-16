f221d9768f042acedf49aca11866a77e2747bd9e
Cayde Dixon <cayde.dixon@gmail.com>
2014-02-16 22:36:02 +1100

    DPH update

--- src/main/java/net/cazzar/corelib/network/DynamicPacketHandler.java
+++ src/main/java/net/cazzar/corelib/network/DynamicPacketHandler.java
@@ -1,13 +1,16 @@
 package net.cazzar.corelib.network;
 
 import cpw.mods.fml.common.network.FMLIndexedMessageToMessageCodec;
+import cpw.mods.fml.common.network.NetworkRegistry;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import net.cazzar.corelib.network.packets.IPacket;
+import net.cazzar.corelib.util.ClientUtil;
+import net.cazzar.corelib.util.CommonUtil;
+import net.minecraft.network.NetHandlerPlayServer;
 
 public class DynamicPacketHandler extends FMLIndexedMessageToMessageCodec<IPacket> {
     public DynamicPacketHandler(Class<? extends IPacket>... packets) {
-
         for (int i = 0; i > packets.length; i++)
             addDiscriminator(i, packets[i]);
 
@@ -20,7 +23,15 @@ public class DynamicPacketHandler extends FMLIndexedMessageToMessageCodec<IPacke
 
     @Override
     public void decodeInto(ChannelHandlerContext ctx, ByteBuf bytes, IPacket packet) {
-
         packet.read(bytes);
+        switch (CommonUtil.getSide()) {
+            case CLIENT:
+                packet.handleClient(ClientUtil.mc().thePlayer);
+                break;
+            case SERVER:
+                packet.handleServer(((NetHandlerPlayServer) ctx.channel().attr(NetworkRegistry.NET_HANDLER).get()).playerEntity);
+                break;
+        }
+//        packet.read(bytes);
     }
 }
--- src/main/java/net/cazzar/corelib/network/packets/IPacket.java
+++ src/main/java/net/cazzar/corelib/network/packets/IPacket.java
@@ -1,9 +1,14 @@
 package net.cazzar.corelib.network.packets;
 
 import io.netty.buffer.ByteBuf;
+import net.minecraft.entity.player.EntityPlayer;
 
 public interface IPacket {
-    public abstract void read(ByteBuf in);
+    public void read(ByteBuf in);
 
-    public abstract void write(ByteBuf out);
+    public void handleClient(EntityPlayer player);
+
+    public void handleServer(EntityPlayer player);
+
+    public void write(ByteBuf out);
 }
