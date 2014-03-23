4ac3008d4988e5f76fa74f7825d8d9ca93c7823d
Cayde Dixon <cayde.dixon@gmail.com>
2014-03-24 00:49:49 +1100

    Rework SoundSystemHelper to use the old method Because fuck minecraft!

--- src/main/java/net/cazzar/corelib/lib/SoundSystemHelper.java
+++ src/main/java/net/cazzar/corelib/lib/SoundSystemHelper.java
@@ -20,17 +20,22 @@ package net.cazzar.corelib.lib;
 import com.google.common.collect.HashBiMap;
 import com.google.common.collect.Lists;
 import cpw.mods.fml.common.ObfuscationReflectionHelper;
+import cpw.mods.fml.relauncher.ReflectionHelper;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.client.audio.ISound;
-import net.minecraft.client.audio.SoundHandler;
-import net.minecraft.client.audio.SoundManager;
+import net.minecraft.client.audio.*;
 import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemRecord;
 import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import paulscode.sound.SoundSystem;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
@@ -55,7 +60,7 @@ public class SoundSystemHelper {
     }
 
     public static SoundSystem getSoundSystem() {
-       return ObfuscationReflectionHelper.getPrivateValue(SoundManager.class, getSoundManager(), "sndSystem", "field_148620_e");
+        return ObfuscationReflectionHelper.getPrivateValue(SoundManager.class, getSoundManager(), "sndSystem", "field_148620_e");
     }
 
     /**
@@ -66,18 +71,47 @@ public class SoundSystemHelper {
      * @param y      the y pos
      * @param z      the z pos
      */
-    public static void playRecord(World world, String record, int x, int y, int z) {
-        world.playRecord(record, x, y, z);
+    public static void playRecord(World world, String record, int x, int y, int z, String identifier) {
+        ItemRecord itemrecord = ItemRecord.getRecord(record);
+
+        ResourceLocation resource;
+        if (itemrecord == null) {
+            return;
+        }
+
+        mc().ingameGUI.setRecordPlayingMessage(itemrecord.getRecordNameLocal());
+        resource = itemrecord.getRecordResource(record);
+        if (resource == null) return;
+
+        if (getSoundSystem().playing(identifier))
+            getSoundSystem().stop(identifier);
+
+
+        SoundEventAccessorComposite sound = getSoundHandler().getSound(resource);
+        float f1 = 16F;
+
+        SoundPoolEntry soundpoolentry = sound.func_148720_g();
+
+        SoundCategory soundcategory = sound.getSoundCategory();
+        float volume = (float) MathHelper.clamp_double((double)f1 * soundpoolentry.getVolume() * (double)mc().gameSettings.getSoundLevel(soundcategory), 0.0D, 1.0D);
+        float pitch = (float) MathHelper.clamp_double((double)f1 * soundpoolentry.getVolume() * (double)mc().gameSettings.getSoundLevel(soundcategory), 0.0D, 1.0D);
+        ResourceLocation resourcelocation = soundpoolentry.getSoundPoolEntryLocation();
+
+        getSoundSystem().newStreamingSource(false, identifier, getURLForSoundResource(resourcelocation), resourcelocation.toString(),false, x, y, z, ISound.AttenuationType.LINEAR.getTypeInt(), f1);
+        getSoundSystem().setPitch(identifier, pitch);
+        getSoundSystem().setVolume(identifier, volume);
+        getSoundSystem().play(identifier);
     }
 
     /**
      * stop any sounds playing at the coords.
      *
-     * @param world the world that it is playing in
+     * @param identifier the identifier of the playing sound
      */
-    public static void stop(ChunkCoordinates coordinates) {
-       stop(mc().renderGlobal, coordinates);
+    public static void stop(String identifier) {
+        getSoundSystem().stop(identifier);
     }
+
     /**
      * stop any sounds playing at the coords.
      *
@@ -93,12 +127,11 @@ public class SoundSystemHelper {
     /**
      * Check if something is playing at the coords
      *
-     * @param coordinates the coordinates of the "player"
-     *
+     * @param identifier the identifier of the playing sound
      * @return if the {@link SoundSystem} is playing with that identifier.
      */
-    public static boolean isPlaying(ChunkCoordinates coordinates) {
-        return isPlaying(mc().renderGlobal, coordinates);
+    public static boolean isPlaying(String identifier) {
+        return getSoundSystem().playing(identifier);
     }
 
     /**
@@ -106,7 +139,6 @@ public class SoundSystemHelper {
      *
      * @param world       the world that it is playing in
      * @param coordinates the coordinates of the "player"
-     *
      * @return if the {@link SoundSystem} is playing with that identifier.
      */
     public static boolean isPlaying(RenderGlobal world, ChunkCoordinates coordinates) {
@@ -116,7 +148,7 @@ public class SoundSystemHelper {
     }
 
     public static String getChannel(ISound sound) {
-        HashBiMap<String, ISound> playingSounds =  ObfuscationReflectionHelper.getPrivateValue(SoundManager.class, getSoundManager(), "playingSounds", "field_148629_h");
+        HashBiMap<String, ISound> playingSounds = ObfuscationReflectionHelper.getPrivateValue(SoundManager.class, getSoundManager(), "playingSounds", "field_148629_h");
         return playingSounds.inverse().get(sound);
     }
 
@@ -128,6 +160,18 @@ public class SoundSystemHelper {
     @SideOnly(Side.CLIENT)
     public static void registerRecord(String s) {
         //    getSoundManager().soundPoolStreaming.addSound(s);
+    }
+
+    public static URL getURLForSoundResource(ResourceLocation location){
+        Method m = ReflectionHelper.findMethod(SoundManager.class, getSoundManager(), new String[]{"getURLForSoundResource", "func_148612_a"}, ResourceLocation.class);
+        try {
+            return (URL) m.invoke(getSoundManager(), location);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @SideOnly(Side.CLIENT)
